a64110e5a8fc586f59ff4c0cee1801ab98170d57
Paul Godavari <paulgod@gmail.com>
2022-02-09 17:43:20 -0600

    Clean up test code

--- Test/MathLibTest/MathLibTest.mm
+++ Test/MathLibTest/MathLibTest.mm
@@ -26,6 +26,9 @@ bool WithinMarginOfError(float test_value, float expected_value, float error_mar
 }
 
 
+//--------------------------------------------------------------------------------------------------
+// Unit testing
+
 @interface MathLibTest : XCTestCase
 @end
 
@@ -34,26 +37,12 @@ bool WithinMarginOfError(float test_value, float expected_value, float error_mar
 
 - (void) setUp
 {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Put setup code here. Called before the invocation of each test method in the class.
 }
 
 - (void) tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void) testExample
-{
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void) testVector3UnitVectorMagnitude
-{
-    v3 p = { 2.0, 2.0, 2.0 };
-    v3 unit = UnitVector(p);
-    float magnitude = unit.Magnitude();
-    XCTAssertTrue(WithinMarginOfError(magnitude, 1.0));
+    // Put teardown code here. Called after the invocation of each test method in the class.
 }
 
 // - (void) testPerformanceExample
@@ -63,5 +52,13 @@ bool WithinMarginOfError(float test_value, float expected_value, float error_mar
 //         // Put the code you want to measure the time of here.
 //     }];
 // }
+
+- (void) testVector3UnitVectorMagnitude
+{
+    v3 p = { 2.0, 2.0, 2.0 };
+    v3 unit = UnitVector(p);
+    float magnitude = unit.Magnitude();
+    XCTAssertTrue(WithinMarginOfError(magnitude, 1.0));
+}
 
 @end
